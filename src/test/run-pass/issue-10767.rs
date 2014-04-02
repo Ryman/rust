304d82a0a1961e1eb41179f9ebda37d9d9873a1a
Alex Crichton <alex@alexcrichton.com>
2014-04-02 09:47:11 -0700

    syntax: Tweak parsing lifetime bounds on closures
    In summary these are some example transitions this change makes:
    
        'a ||       => ||: 'a
        proc:Send() => proc():Send
    
    The intended syntax for closures is to put the lifetime bound not at the front
    but rather in the list of bounds. Currently there is no official support in the
    AST for bounds that are not 'static, so this case is currently specially handled
    in the parser to desugar to what the AST is expecting. Additionally, this moves
    the bounds on procedures to the correct position, which is after the argument
    list.
    
    The current grammar for closures and procedures is:
    
        procedure := 'proc' [ '<' lifetime-list '>' ] '(' arg-list ')'
                            [ ':' bound-list ] [ '->' type ]
        closure := [ 'unsafe' ] ['<' lifetime-list '>' ] '|' arg-list '|'
                            [ ':' bound-list ] [ '->' type ]
        lifetime-list := lifetime | lifetime ',' lifetime-list
        arg-list := ident ':' type | ident ':' type ',' arg-list
        bound-list := bound | bound '+' bound-list
        bound := path | lifetime
    
    This does not currently handle the << ambiguity in `Option<<'a>||>`, I am
    deferring that to a later patch. Additionally, this removes the support for the
    obsolete syntaxes of ~fn and &fn.
    
    Closes #10553
    Closes #10767
    Closes #11209
    Closes #11210
    Closes #11211

--- src/test/run-pass/issue-10767.rs
+++ src/test/run-pass/issue-10767.rs
@@ -1,4 +1,4 @@
-// Copyright 2013 The Rust Project Developers. See the COPYRIGHT
+// Copyright 2014 The Rust Project Developers. See the COPYRIGHT
 // file at the top-level directory of this distribution and at
 // http://rust-lang.org/COPYRIGHT.
 //
@@ -8,14 +8,8 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-fn bar(blk: ||:'static) {
-}
-
-fn foo(x: &()) {
-    bar(|| { //~ ERROR cannot infer an appropriate lifetime
-        let _ = x;
-    })
-}
-
 fn main() {
+    fn f() {
+    };
+    let _: ~fn() = ~f;
 }
